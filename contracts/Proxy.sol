cfc39fa25f5aa697c130e71cbb7e42988973a832
Altaergo <73134512+Altaergo@users.noreply.github.com>
2021-05-28 23:47:58 +0300

    deployjs

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -4,10 +4,20 @@ pragma solidity 0.8.1;
 
 contract Proxy {
     constructor(bytes memory constructData, address contractLogic) {
+        //Code position in storage is keccak256(PROXIABLE) = ""
         assembly {
             sstore()
         }
         (bool success, bytes memory result) =
-            contractLogic.delegatecall(constructData);
+            contractLogic.delegatecall(constructData); // solium-disable-line
+        require(success, "Construction true");
+    }
+
+    fallback() external payable {
+        assembly { // solium-disable-line
+        let contractLogic "= sload()  
+        calldatacopy(0x0, 0x0, calldatasize())
+        let success
+        }
     }
 }
