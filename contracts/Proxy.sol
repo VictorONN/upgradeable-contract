d73be827a613a7a41725365166d057b3a8477829
Altaergo <73134512+Altaergo@users.noreply.github.com>
2021-06-03 06:09:45 +0300

    complete

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -17,7 +17,59 @@ contract Proxy {
         assembly { // solium-disable-line
         let contractLogic "= sload()  
         calldatacopy(0x0, 0x0, calldatasize())
-        let success
+        let success := delegatecall(sub(gas(), 10000), contractLogic, 0x0, calldatasize(), 0, 0)
+        let retSz := returndatasize()
+        returndatacopy(0, 0, retSz)
+        switch success
+        case 0 {
+            revert(0, retSz)
         }
+        default{
+            return(0, retSz)
+        }
+        }
+    }
+}
+
+contract Proxiable {
+    // Code position in storage is keccak256("PROXIABLE) = "0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7"
+
+    function updateCodeAddress(address newAddress) internal {
+        require(bytes32(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7) == Proxiable(newAddress).proxiableUUID(), "Not compatible");
+        assembly {
+            //solium-disable-line
+            sstore(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, newAddress)
+        }
+    }
+
+    function proxiableUUID() public pure returns (bytes32) {
+        return 0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7;
+    }
+}
+
+contract MyContract {
+    address public owner;
+    uint public myUint;
+
+    function constructor1() public {
+        require(owner == address(0), "Already initialized");
+        owner = msg.sender;
+    }
+
+    function increment() public {
+        //require(msg.sender == owner, "Only the owner can increment"); //someone forgot to uncomment
+        myUint++;
+    }
+}
+
+contract MyFinalContract is MyContract, Proxiable {
+
+    function updateCode(address newCode) onlyOwner public {
+        updateCodeAddress(newCode);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner allowed");
+        _;
     }
 }
